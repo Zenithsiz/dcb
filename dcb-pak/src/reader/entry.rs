bd53e223cb0f62c9dcca7b3ed3cfc76d96d071d5
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-03-31 20:44:04 +0100

    Updated some documentation on `PakEntryReader`.

--- dcb-pak/src/reader/entry.rs
+++ dcb-pak/src/reader/entry.rs
@@ -22,27 +22,23 @@ pub struct PakEntryReader<'a, R> {
 	header: Header,
 
 	/// Entry reader
+	///
+	/// Note: This will be seeked to the start of
+	///       this entry's content.
 	reader: &'a mut R,
 }
 
-impl<'a, R> PakEntryReader<'a, R> {
-	/// Returns this entry's header
-	#[must_use]
-	pub const fn header(&self) -> &Header {
-		&self.header
-	}
-}
-
+// Constructor
 impl<'a, R: io::Read> PakEntryReader<'a, R> {
 	/// Deserializes an entry from a reader
 	pub fn from_reader(reader: &'a mut R) -> Result<Option<Self>, FromReaderError> {
 		// Read the header
-		// Note: We do a two-part read so we can quit early if we find `0xffff`
+		// Note: We do a two-part read so we can quit early if we find `0xffffffff`
 		let mut header_bytes = [0u8; 0x8];
 		reader.read_exact(&mut header_bytes[..0x4]).map_err(FromReaderError::ReadHeader)?;
 
-		// If we found `0xFFFF`, this is the final header, return
-		if header_bytes[..0x4] == [0xff, 0xff, 0xff, 0xff] {
+		// If we found `0xffffffff`, this is the final header, return
+		if header_bytes[..0x4] == [0xff; 4] {
 			return Ok(None);
 		}
 
@@ -52,12 +48,23 @@ impl<'a, R: io::Read> PakEntryReader<'a, R> {
 
 		Ok(Some(Self { header, reader }))
 	}
+}
 
+// Getters
+impl<'a, R> PakEntryReader<'a, R> {
+	/// Returns this entry's header
+	#[must_use]
+	pub const fn header(&self) -> &Header {
+		&self.header
+	}
+}
+
+// Read
+impl<'a, R: io::Read> PakEntryReader<'a, R> {
 	/// Returns the contents of this entry
 	#[must_use]
 	pub fn contents(self) -> impl io::Read + 'a {
-		// Note: We left the reader at the start of the read, so
-		//       this will be correct.
+		// Note: Our reader is at the start of the contents
 		<&mut R as io::Read>::take(self.reader, u64::from(self.header.size))
 	}
 }
