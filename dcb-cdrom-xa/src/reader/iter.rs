c483dca032ea5e8b7e64f726d77b5ef9659e77ba
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-04-02 15:17:57 +0100

    Fixed eof condition in `SectorsRangeIter`.

--- dcb-cdrom-xa/src/reader/iter.rs
+++ dcb-cdrom-xa/src/reader/iter.rs
@@ -23,7 +23,14 @@ impl<'a, R: io::Read> Iterator for SectorsRangeIter<'a, R> {
 	type Item = Result<Sector, reader::ReadSectorError>;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		// Read the next sector
-		Some(self.cdrom.read_sector())
+		// Try to read the next sector
+		match self.cdrom.read_sector() {
+			Ok(sector) => Some(Ok(sector)),
+			// If we found eof, stop
+			// FIXME: Unfortunately we don't know if we hit eof with 0 or more in the last buffer,
+			//        we just silently ignore anything past the last sector currently
+			Err(reader::ReadSectorError::Read(err)) if err.kind() == io::ErrorKind::UnexpectedEof => None,
+			Err(err) => Some(Err(err)),
+		}
 	}
 }
