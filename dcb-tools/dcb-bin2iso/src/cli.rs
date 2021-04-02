ea9e4bb34d5194beabec5bc30cb32bd1cf838757
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-04-02 15:19:52 +0100

    Added `dcb-bin2iso` binary.

---
+++ dcb-tools/dcb-bin2iso/src/cli.rs
@@ -0,0 +1,55 @@
+//! Cli manager
+
+// Imports
+use clap::{App as ClapApp, Arg as ClapArg};
+use std::path::{Path, PathBuf};
+
+/// Data from the command line
+#[derive(PartialEq, Clone, Debug)]
+pub struct CliData {
+	/// Input file
+	pub input_file: PathBuf,
+
+	/// The output file
+	pub output_file: PathBuf,
+}
+
+impl CliData {
+	/// Constructs all of the cli data given and returns it
+	pub fn new() -> Self {
+		// Get all matches from cli
+		let matches = ClapApp::new("Bin to Iso")
+			.version("0.1")
+			.author("Filipe [...] <[...]@gmail.com>")
+			.about("Coverts a `.bin` in the CdRom/XA format to a ISO 9660 filesystem file")
+			.arg(ClapArg::with_name("INPUT_FILE").help("The input file to use").required(true).index(1))
+			.arg(
+				ClapArg::with_name("OUTPUT")
+					.help("The file to output to")
+					.long_help("The file to output to. If not specified, a file with the input's name changed by `.iso` will be used")
+					.short("o")
+					.long("output")
+					.takes_value(true)
+					.required(false),
+			)
+			.get_matches();
+
+		// Get the input filename
+		// Note: required
+		let input_file = matches
+			.value_of("INPUT_FILE")
+			.map(Path::new)
+			.map(Path::to_path_buf)
+			.expect("Unable to get required argument `INPUT_FILE`");
+
+		// Try to get the output, else use the input filename + `.bin`
+		let output_file = match matches.value_of("OUTPUT") {
+			Some(output) => PathBuf::from(output),
+			// FIXME: If the user inputs a file that's a `.iso`, this uses the same file as output.
+			None => input_file.with_extension("iso"),
+		};
+
+		// Return the data
+		Self { input_file, output_file }
+	}
+}
---
+++ dcb-tools/dcb-bin2iso/src/main.rs
@@ -0,0 +1,45 @@
+//! CdRom packer
+
+// Modules
+mod cli;
+
+// Imports
+use anyhow::Context;
+use dcb_cdrom_xa::CdRomReader;
+use std::{fs, io::Write, path::Path};
+
+
+fn main() -> Result<(), anyhow::Error> {
+	// Initialize the logger
+	simplelog::TermLogger::init(log::LevelFilter::Info, simplelog::Config::default(), simplelog::TerminalMode::Stderr)
+		.expect("Unable to initialize logger");
+
+	// Get all data from cli
+	let cli::CliData { input_file, output_file } = cli::CliData::new();
+
+	// Try to extract it into a `iso`
+	self::extract_cdrom_xa(&input_file, &output_file).context("Unable to extract file")?;
+
+	Ok(())
+}
+
+/// Packs a file into a `CdRom/XA`
+fn extract_cdrom_xa(input_file: &Path, output_file: &Path) -> Result<(), anyhow::Error> {
+	// Open the input file
+	let input_file = fs::File::open(input_file).context("Unable to open input file")?;
+	let mut input_file = CdRomReader::new(input_file);
+
+	// Create the output file
+	let mut output_file = fs::File::create(output_file).context("Unable to create output file")?;
+
+	// Read all sectors
+	for sector in input_file.read_sectors() {
+		let sector = sector.context("Unable to read sector")?;
+
+		println!("{:?}", sector.header);
+
+		output_file.write_all(&sector.data).context("Unable to write data")?;
+	}
+
+	Ok(())
+}
